e9a3e79945667903ef186a2a6464e1ece8d8e20e
Elvio <elviobg@gmail.com>
2018-05-02 16:53:08 -0300

    Adiciona o projeto 2.3
    Qual página mais popular e qual o número de acessos da mesma

---
+++ 2.3_paginas_populares/mapper.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+# Format of each line is Common Log Format:
+#
+# We want elements 1 and 5
+# We need to write them out to standard output, separated by a tab
+
+import sys
+import re
+
+p = re.compile('([^ ]*) ([^ ]*) ([^ ]*) \[([^]]*)\] "([^"]*)" ([^ ]*) ([^ ]*)')
+file = open('/home/training/udacity_training/data/1000_logs.txt', 'r')
+toRemove = 'http://www.the-associates.co.uk'
+
+for line in sys.stdin:
+    m = p.match(line.strip())
+    if not m:
+        continue
+    host, id, user, date, request, status, size = m.groups()
+    data = request.strip().split(" ")
+    if len(data) == 3:
+      method, path, http = data
+      
+      if toRemove in path:
+        path = path.replace(toRemove, '')
+      print "{0}\t{1}".format(path, 1)
